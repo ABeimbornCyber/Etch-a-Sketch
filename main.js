2231c5f3da7cf5035d155f3a937d696a824ad258
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-20 11:25:13 -0500

    Adding grid width input box

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const grid_container = document.querySelector(".container");
+let grid_width = 16;
 
 
-
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < grid_width; i++){
     const grid_row = document.createElement("div");
     grid_row.classList.add("grid_row");
     grid_container.appendChild(grid_row);
     
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < grid_width; j++){
         const grid_section = document.createElement("div");
         grid_section.classList.add("square");
         grid_row.appendChild(grid_section);
