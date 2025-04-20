1b3d07d1683045b4bb02364f96cc4ad2f56931ff
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-20 18:10:12 -0500

    Adding Crazy Mode

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const grid_container = document.querySelector(".container");
 let grid_width = 16;
 const grid_input = document.querySelector("#grid_width");
-
+const crazyMode = document.querySelector("#crazyMode");
+const grid_width_px = 600;
+let crazy = false;
 draw_grid();
 function draw_grid(width=16){
     if(width < 1){
@@ -11,7 +13,7 @@ function draw_grid(width=16){
         width=100;
         grid_input.value = 100;
     }
-    grid_container.innerHTML = '';
+    grid_container.textContent = '';
     for(let i = 0; i < width; i++){
         const grid_row = document.createElement("div");
         grid_row.classList.add("grid_row");
@@ -21,15 +23,39 @@ function draw_grid(width=16){
             const grid_section = document.createElement("div");
             grid_section.classList.add("square");
             grid_row.appendChild(grid_section);
-            
+            width_str = (grid_width_px/width)+"px";
+            grid_section.style.width = width_str;
+            grid_section.style.height = width_str;
             grid_section.addEventListener("mouseover", function(){
-                grid_section.classList.add("hovered");
+                if(crazy){
+                    let r = Math.floor(Math.random()*256);
+                    let g = Math.floor(Math.random()*256);
+                    let b = Math.floor(Math.random()*256);
+                    let rgb_string = "rgb("+r+","+g+","+b+")";
+                    grid_section.style.backgroundColor = rgb_string;
+                }else{
+                    grid_section.style.backgroundColor = "green";
+                }
+                
             });
             
         }
     }
 }
-
+crazyMode.addEventListener('click', function(){
+    if(!crazy){
+        crazyMode.style.color = "lightgreen";
+        crazyMode.style.backgroundColor = "red";
+        crazyMode.style.borderColor = "olive";
+        crazy = true;
+    }else {
+        crazyMode.style.backgroundColor= "orchid";
+        crazyMode.style.color =  "gold";
+        crazyMode.style.borderColor = "chartreuse";
+        crazy = false;
+    }
+    
+});
 
 grid_input.addEventListener('input', function(e){
     
