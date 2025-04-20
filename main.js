fff3de2dcb7dfd8d16a4590e1db021ad6597550b
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-20 07:59:43 -0500

    Adding much needed styling

--- main.js
+++ main.js
@@ -6,19 +6,14 @@ for(let i = 0; i < 16; i++){
     const grid_row = document.createElement("div");
     grid_row.classList.add("grid_row");
     grid_container.appendChild(grid_row);
+    
     for(let j = 0; j < 16; j++){
         const grid_section = document.createElement("div");
         grid_section.classList.add("square");
         grid_row.appendChild(grid_section);
+        
+        
     }
 }
-
-
-
-
-
-
-
-
 
 
