9389b77898e3039a2047c48ce52e97c25698390d
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-20 11:44:43 -0500

    Adding custom grid size

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const grid_container = document.querySelector(".container");
 let grid_width = 16;
-
-
-for(let i = 0; i < grid_width; i++){
-    const grid_row = document.createElement("div");
-    grid_row.classList.add("grid_row");
-    grid_container.appendChild(grid_row);
-    
-    for(let j = 0; j < grid_width; j++){
-        const grid_section = document.createElement("div");
-        grid_section.classList.add("square");
-        grid_row.appendChild(grid_section);
-        grid_section.addEventListener("mouseover", function(){
-            grid_section.classList.add("hovered");
-        });
+const grid_input = document.querySelector("#grid_width");
+draw_grid();
+function draw_grid(width=16){
+    for(let i = 0; i < width; i++){
+        const grid_row = document.createElement("div");
+        grid_row.classList.add("grid_row");
+        grid_container.appendChild(grid_row);
         
+        for(let j = 0; j < width; j++){
+            const grid_section = document.createElement("div");
+            grid_section.classList.add("square");
+            grid_row.appendChild(grid_section);
+            grid_section.addEventListener("mouseover", function(){
+                grid_section.classList.add("hovered");
+            });
+            
+        }
     }
 }
+
+
+grid_input.addEventListener('input', function(e){
+    grid_width = this.value;
+});
